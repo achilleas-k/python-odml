914b918b9c38388726a709d205c091425ee865f0
Achilleas Koutsou <achilleas.k@gmail.com>
2017-01-14 21:47:04 +0100

    [setup] Add dependencies and clean up
    Bumped version to 1.3dev

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
-
-import sys
-import os
-import glob
-
 try:
     from setuptools import setup
 except ImportError as ex:
     from distutils.core import setup
-
-kwargs = {}
 
 packages = [
     'odml',
@@ -18,11 +11,12 @@ packages = [
 
 
 setup(name='odML',
-      version='1.2',
+      version='1.3dev',
       description='open metadata Markup Language',
       author='Hagen Fritsch',
       author_email='fritsch+gnode@in.tum.de',
       url='http://www.g-node.org/projects/odml',
       packages=packages,
       test_suite='test',
-      **kwargs)
+      install_requires=["enum", "lxml"],
+      )
